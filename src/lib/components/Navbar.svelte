84f9ffcf0cf97c723e5af6e66f50b6803bfd4e64
ozenui <enzorma@protonmail.com>
2025-06-28 22:20:45 +0200

    Add Navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import AppleIcon from '$lib/assets/icon-apple.svg';
+	import BlueskyIcon from '$lib/assets/icon-bluesky.svg';
+	import GithubIcon from '$lib/assets/icon-github.svg';
+	import ContraIcon from '$lib/assets/icon-contra.svg';
+
+	const currentDate = new Date();
+
+	const formattedDate = currentDate
+		.toLocaleString('en-US', {
+			weekday: 'short',
+			month: 'short',
+			day: 'numeric',
+			hour: 'numeric',
+			minute: '2-digit',
+			hour12: true
+		})
+		.replace(/,/, '')
+		.replace(/,/, '  ');
+
+	interface NavEntry {
+		label: string;
+		href: string;
+	}
+
+	const navEntries: NavEntry[] = [
+		{
+			label: 'ozenui',
+			href: '/'
+		},
+		{
+			label: 'About',
+			href: '/about'
+		},
+		{
+			label: 'Projects',
+			href: '/projects'
+		},
+		{
+			label: 'Blog',
+			href: '/blog'
+		},
+		{
+			label: 'Contact',
+			href: '/contact'
+		}
+	];
+</script>
+
+<nav class="flex items-center justify-between p-2 text-[13px]">
+	<div class="flex items-center gap-4">
+		<a href="/">
+			<img src={AppleIcon} alt="Home" />
+		</a>
+
+		{#each navEntries as entry}
+			<a
+				href={entry.href}
+				class={`font-medium text-neutral-300 ${page.url.pathname === entry.href ? 'font-bold text-white' : ''}`}
+			>
+				{entry.label}
+			</a>
+		{/each}
+	</div>
+	<div class="flex items-center gap-4">
+		<a href="https://bsky.app/profile/ozenui.com" target="_blank">
+			<img src={BlueskyIcon} alt="Bluesky" />
+		</a>
+		<a href="https://github.com/ozenui/ozenui" target="_blank">
+			<img src={GithubIcon} alt="GitHub" />
+		</a>
+		<a href="https://contra.com/enzoromera" target="_blank">
+			<img src={ContraIcon} alt="Contra" />
+		</a>
+
+		<p class="font-semibold whitespace-pre text-white">
+			{formattedDate}
+		</p>
+	</div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import Navbar from '$lib/components/Navbar.svelte';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
 {@render children()}
+
+<main class="h-screen w-screen bg-black p-2 text-neutral-50">
+	<Navbar></Navbar>
+</main>
