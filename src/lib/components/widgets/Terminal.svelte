2b605dab1cada9a6ec1e0592c96221e207cacd36
ozenui <enzorma@protonmail.com>
2025-06-29 11:33:29 +0200

    Add neofetch enter mask animation

--- src/lib/components/widgets/Terminal.svelte
+++ src/lib/components/widgets/Terminal.svelte
@@ -3,8 +3,14 @@
 	import { quadOut } from 'svelte/easing';
 	import { fly } from 'svelte/transition';
 
-	let textarea: HTMLTextAreaElement | undefined = $state(undefined);
 	let { children } = $props();
+
+	let textarea: HTMLTextAreaElement | undefined = $state(undefined);
+	let mounted = $state(false);
+
+	$effect(() => {
+		mounted = true;
+	});
 
 	function autoGrow() {
 		if (!textarea) return;
@@ -16,72 +22,77 @@
 <main
 	class="flex h-full w-full flex-col items-start justify-start gap-5 overflow-hidden rounded-[10px] border border-white/10 bg-white/5 p-4 text-left font-mono text-[13px] transition-all duration-300 ease-out"
 >
-	<section
-		in:fly={{ y: -20, easing: quadOut, delay: 1000 }}
-		class="relative inline-flex items-stretch gap-5 self-stretch"
-	>
-		<!-- Image -->
-		<div class="flex-shrink-0">
-			<img class="aspect-square h-[300px] bg-[#1D1D1D] object-contain" src={Avatar} alt="Avatar" />
-		</div>
+	{#if mounted}
+		<section
+			class="animate-mask-reveal relative inline-flex items-stretch gap-5 self-stretch overflow-hidden"
+		>
+			<!-- Image -->
+			<div class="flex-shrink-0">
+				<img
+					class="aspect-square h-[300px] bg-[#1D1D1D] object-contain"
+					src={Avatar}
+					alt="Avatar"
+				/>
+			</div>
 
-		<!-- Info list -->
-		<ul class="flex flex-1 flex-col justify-center gap-2">
-			<li class="inline-flex items-center gap-2">
-				<div class="font-normal text-[#ffefaf]">ozenui</div>
-			</li>
+			<!-- Info list -->
+			<ul class="flex flex-1 flex-col justify-center gap-2">
+				<li class="inline-flex items-center gap-2">
+					<div class="font-normal text-[#ffefaf]">ozenui</div>
+				</li>
 
-			<li class="inline-flex items-center gap-2">
-				<div class="font-normal text-[#ffefaf]">-------</div>
-			</li>
+				<li class="inline-flex items-center gap-2">
+					<div class="font-normal text-[#ffefaf]">-------</div>
+				</li>
 
-			<li class="inline-flex items-center gap-2">
-				<div class="font-normal text-[#ffefaf]">Name:</div>
-				<div class="font-normal text-[#f7afff]">Enzo Romera</div>
-			</li>
+				<li class="inline-flex items-center gap-2">
+					<div class="font-normal text-[#ffefaf]">Name:</div>
+					<div class="font-normal text-[#f7afff]">Enzo Romera</div>
+				</li>
 
-			<li class="inline-flex items-center gap-2">
-				<div class="font-normal text-[#ffefaf]">Nickname:</div>
-				<div class="font-normal text-[#f7afff]">ozenui</div>
-			</li>
+				<li class="inline-flex items-center gap-2">
+					<div class="font-normal text-[#ffefaf]">Nickname:</div>
+					<div class="font-normal text-[#f7afff]">ozenui</div>
+				</li>
 
-			<li class="inline-flex items-center gap-2">
-				<div class="font-normal text-[#ffefaf]">Location:</div>
-				<div class="font-normal text-[#f7afff]">France</div>
-			</li>
+				<li class="inline-flex items-center gap-2">
+					<div class="font-normal text-[#ffefaf]">Location:</div>
+					<div class="font-normal text-[#f7afff]">France</div>
+				</li>
 
-			<li class="inline-flex items-center gap-2">
-				<div class="font-normal text-[#ffefaf]">Languages:</div>
-				<div class="font-normal text-[#f7afff]">fr, en</div>
-			</li>
+				<li class="inline-flex items-center gap-2">
+					<div class="font-normal text-[#ffefaf]">Languages:</div>
+					<div class="font-normal text-[#f7afff]">fr, en</div>
+				</li>
 
-			<li class="inline-flex items-center gap-2">
-				<div class="font-normal text-[#ffefaf]">Currently:</div>
-				<div class="font-normal text-[#f7afff]">Design engineer @ FullEnrich</div>
-			</li>
+				<li class="inline-flex items-center gap-2">
+					<div class="font-normal text-[#ffefaf]">Currently:</div>
+					<div class="font-normal text-[#f7afff]">Design engineer @ FullEnrich</div>
+				</li>
 
-			<li class="inline-flex items-center gap-2">
-				<div class="font-normal text-[#ffefaf]">Previously:</div>
-				<div class="font-normal text-[#f7afff]">Design engineer @ Sesterce</div>
-			</li>
+				<li class="inline-flex items-center gap-2">
+					<div class="font-normal text-[#ffefaf]">Previously:</div>
+					<div class="font-normal text-[#f7afff]">Design engineer @ Sesterce</div>
+				</li>
 
-			<li class="inline-flex items-center gap-2">
-				<div class="font-normal text-[#ffefaf]">Side project:</div>
-				<div class="font-normal text-[#f7afff]">Blurr (more soon)</div>
-			</li>
+				<li class="inline-flex items-center gap-2">
+					<div class="font-normal text-[#ffefaf]">Side project:</div>
+					<div class="font-normal text-[#f7afff]">Blurr (more soon)</div>
+				</li>
 
-			<li class="inline-flex items-center gap-2">
-				<div class="font-normal text-[#ffefaf]">Freelance seats:</div>
-				<div class="font-normal text-[#f7afff]">1/1</div>
-			</li>
+				<li class="inline-flex items-center gap-2">
+					<div class="font-normal text-[#ffefaf]">Freelance seats:</div>
+					<div class="font-normal text-[#f7afff]">1/1</div>
+				</li>
 
-			<li class="inline-flex w-[400px] flex-wrap items-center pt-4">
-				{#each ['#ff0000', '#ff9000', '#ffdd00', '#94ff00', '#26ff00', '#00ffa1', '#00fffa', '#00bbff', '#0050ff', '#4c00ff', '#8800ff', '#cc00ff', '#ff00d4', '#ff006e', 'white', '#e4e4e4', '#b0b0b0', '#434343', 'black'] as color}
-					<div class="relative h-2 flex-1" style="background-color: {color}"></div>
-				{/each}
-			</li>
-		</ul>
-	</section>
+				<li class="inline-flex w-[400px] flex-wrap items-center pt-4">
+					{#each ['#ff0000', '#ff9000', '#ffdd00', '#94ff00', '#26ff00', '#00ffa1', '#00fffa', '#00bbff', '#0050ff', '#4c00ff', '#8800ff', '#cc00ff', '#ff00d4', '#ff006e', 'white', '#e4e4e4', '#b0b0b0', '#434343', 'black'] as color}
+						<div class="relative h-2 flex-1" style="background-color: {color}"></div>
+					{/each}
+				</li>
+			</ul>
+		</section>
+	{/if}
 
 	<section
 		class="relative flex flex-shrink-0 flex-grow-0 items-start justify-start gap-2 self-stretch"
@@ -99,3 +110,19 @@
 
 	{@render children()}
 </main>
+
+<style>
+	@keyframes maskReveal {
+		0% {
+			clip-path: inset(0 0 100% 0);
+		}
+		100% {
+			clip-path: inset(0 0 0 0);
+		}
+	}
+
+	.animate-mask-reveal {
+		clip-path: inset(0 0 100% 0);
+		animation: maskReveal 0.3s ease-out 0s forwards;
+	}
+</style>
